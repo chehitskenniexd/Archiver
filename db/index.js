3f9911a851cd657f46ca8b4324812e4fd4aa41df
Sia <schen22@gmail.com>
2016-11-19 16:36:23 -0500

    closes #16 and closes #1

--- db/index.js
+++ db/index.js
@@ -2,7 +2,12 @@
 
 const chalk = require('chalk')
 const Sequelize = require('sequelize')
-const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
+// const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
+
+const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`
+
+console.log(chalk.yellow(`Opening database connection to ${url}`));
+
 
 // Require all the models in our database
 const Blob = require('./models/blob');
@@ -26,26 +31,13 @@ Blob.hasMany(File);
 File.belongsTo(Blob);
 
 // create the database instance
-const db = new Sequelize(DATABASE_URI, {
+const db = module.exports = new Sequelize(url, {
   logging: false, // set to console.log to see the raw SQL queries
   native: true // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
 // sync the db, creating it if necessary
-function sync(force=app.isTesting) {
-  return db.sync({force: false})
-    .then(ok => console.log(`Synced models to db ${url}`))
+db.sync({force: true})
+    .then(ok => console.log(chalk.green(`Synced models to db ${url}`)))
     .catch(fail => {
-      if (app.isProduction) {
-        console.error(fail)
-        return // Don't do this auto-create nonsense in prod
-      }
-      // Otherwise, do this autocreate nonsense
-      console.log(`Creating database ${name}...`)
-      return new Promise((resolve, reject) =>
-        require('child_process').exec(`createdb "${name}"`, resolve)
-      ).then(() => sync(true))
-    });
-}
-
-db.didSync = sync();
+      console.error(chalk.red('Failed db sync' + fail));
