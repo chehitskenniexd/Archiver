bd9dceba674c1435b4b86da18e93273e75a02f21
Kenneth Moy <moykenneth91@gmail.com>
2016-11-19 14:57:19 -0500

    finished writing a util to init a new project

---
+++ utilities/gitActions.js
@@ -0,0 +1,24 @@
+
+
+export function initNewProject(dirName) {
+    // Sets the owner/author of this project 
+
+    // Check for a valid directory
+    // fs.statSync() returns a valid fs.Stats object if exists, else and error (try/catch)
+    try {
+        fs.statSync(dirName).isDirectory();
+    } catch (err) {
+        console.log('Invalide directory: ', err);
+        return false;
+    }
+
+    // Check for hidden directory
+    // Create the hidden directory in the projeect directory
+    const archiveDir = `${dirName}/.archive`;
+    try {
+        fs.statSync(archiveDir).isDirectory();
+    } catch (err) {
+        fs.mkdirSync(archiveDir);
+    }
+    return true;
+}
