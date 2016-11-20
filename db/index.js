b6b2a41dd317bd0a093181a42fcc097023dfd738
Kenneth Moy <moykenneth91@gmail.com>
2016-11-20 15:09:50 -0500

    fixed issues where db was not being created Missing db.syinc in index as well as bad path

--- db/index.js
+++ db/index.js
@@ -11,24 +11,24 @@ const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`
 console.log(chalk.yellow(`Hello, Opening database connection to ${url}`));
 console.log("url?", url)
 // create the database instance
-const db = new Sequelize(url, {
+const db = module.exports = new Sequelize(url, {
   // logging: true, // set to console.log to see the raw SQL queries
   native: true // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
 // pull in our models
-//require('./models');
+require('../db/models');
 
 console.log("DB??????", db)
 
 // sync the db, creating it if necessary
-db.sync({force: true})
-    .then(ok => console.log(chalk.green(`Synced models to db ${url}`)))
-    .catch(fail => {
-      // console.error(chalk.red('Failed db sync' + fail))
-      // Otherwise, do this autocreate nonsense
-      console.log(`Creating database ${name}...`)
-      return new Promise((resolve, reject) =>
-        require('child_process').exec(`createdb "${name}"`, resolve)
-      ).then(() => sync(true))
-    });
+db.sync({ force: true })
+  .then(ok => console.log(chalk.green(`Synced models to db ${url}`)))
+  .catch(fail => {
+    // console.error(chalk.red('Failed db sync' + fail))
+    // Otherwise, do this autocreate nonsense
+    console.log(`Creating database ${name}...`)
+    return new Promise((resolve, reject) =>
+      require('child_process').exec(`createdb "${name}"`, resolve)
+    ).then(() => db.sync(true))
+  });
