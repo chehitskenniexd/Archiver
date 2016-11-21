7882fbb896dbf64b16b3bcb04ad7dfcd0fcda739
Hannah Cain <hcain23@gmail.com>
2016-11-21 00:02:29 -0500

    added commit, project, and user seeds

---
+++ db/seeds/user.js
@@ -0,0 +1,126 @@
+
+let users = [
+    {
+        first_name: 'Bob',
+        last_name: 'Williams',
+        email: 'bob@williams.com',
+        password: 'green'
+    },
+    {
+        first_name: 'Barry',
+        last_name: 'Smith',
+        email: 'barry@smith.com',
+        password: 'blue'
+    },
+    {
+        first_name: 'Bernard',
+        last_name: 'Jones',
+        email: 'bernard@jones.com',
+        password: 'purple'
+    },
+    {
+        first_name: 'Beatrice',
+        last_name: 'Johnson',
+        email: 'red',
+        password: 'beatrice@johnson.com'
+    },
+    {
+        first_name: 'Billy',
+        last_name: 'Browne',
+        email: 'billy@browne.com',
+        password: 'orange'
+    },
+    {
+        first_name: 'Bella',
+        last_name: 'Miller',
+        email: 'bella@miller.com',
+        password: 'yellow'
+    },
+    {
+        first_name: 'Bethany',
+        last_name: 'Davis',
+        email: 'bethany@davis.com',
+        password: 'black'
+    },
+    {
+        first_name: 'Brian',
+        last_name: 'Taylor',
+        email: 'brian@taylor.com',
+        password: 'white'
+    },
+    {
+        first_name: 'Bernadette',
+        last_name: 'Wilson',
+        email: 'bernadette@wilson.com',
+        password: 'brown'
+    },
+    {
+        first_name: 'Betsy',
+        last_name: 'White',
+        email: 'betsey@white.com',
+        password: 'indigo'
+    },
+    {
+        first_name: 'Bertha',
+        last_name: 'Hill',
+        email: 'bertha@hill.com',
+        password: 'pink'
+    },
+    {
+        first_name: 'Brittany',
+        last_name: 'Thomas',
+        email: 'brittany@thomas.com',
+        password: 'aqua'
+    },
+    {
+        first_name: 'Bailey',
+        last_name: 'Clark',
+        email: 'bailey@clarke.com',
+        password: 'fuschia'
+    },
+    {
+        first_name: 'Brooke',
+        last_name: 'Jackson',
+        email: 'brooke@jackson.com',
+        password: 'turquoise'
+    },
+    {
+        first_name: 'Brenda',
+        last_name: 'Allen',
+        email: 'brenda@allen.com',
+        password: 'chartreuse'
+    },
+    {
+        first_name: 'Barb',
+        last_name: 'Evans',
+        email: 'barb@evans.com',
+        password: 'lavender'
+    },
+    {
+        first_name: 'Ben',
+        last_name: 'Martin',
+        email: 'ben@martin.com',
+        password: 'lilac'
+    },
+    {
+        first_name: 'Brad',
+        last_name: 'Wright',
+        email: 'brad@wright.com',
+        password: 'magenta'
+    },
+    {
+        first_name: 'Bruce',
+        last_name: 'King',
+        email: 'bruce@king.com',
+        password: 'vermilion'
+    },
+    {
+        first_name: 'Brett',
+        last_name: 'Adams',
+        email: 'brett@adams.com',
+        password: 'violet'
+    }
+
+]
+
+module.exports = users
