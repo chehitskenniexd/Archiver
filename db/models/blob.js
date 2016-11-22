344c1c4a4b77acd3af589eea2da35cefdf798ef1
Kenneth Moy <moykenneth91@gmail.com>
2016-11-22 01:15:01 -0500

    redid yarn install and removed blob logs

--- db/models/blob.js
+++ db/models/blob.js
@@ -1,8 +1,6 @@
 'use strict';
-console.log("BLOG FILE DIRNAME " + __dirname)
 const Sequelize = require('sequelize');
 const db = require('../index.js');
-console.log(db)
 const Blob = db.define('blob', {
   author: {
     type: Sequelize.STRING,
