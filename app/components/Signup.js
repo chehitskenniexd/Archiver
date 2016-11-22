9c5c5306271b3adad4e188b1c312665801e6ca37
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-11-22 11:23:06 -0500

    restarting my computuer

--- app/components/Signup.js
+++ app/components/Signup.js
@@ -6,9 +6,25 @@ import styles from './Signup.css';
 export default class Signup extends Component {
   constructor(props) {
     super(props)
+    // this.state = {
+    //   password: "",
+    //   confirmPassword: ""
+    // }
 
+    // this.changePassword = this.changePassword.bind(this);
+    // this.changeConfirmPassword = this.changeConfirmPassword.bind(this);
     this.checkPasswordMatch = this.checkPasswordMatch.bind(this);
   }
+
+  // changePassword (evt) {
+  //   evt.preventDefault();
+  //   this.setState({password: evt.target.value})
+  // }
+
+  // changeConfirmPassword(evt) {
+  //   evt.preventDefault();
+  //   this.setState({confirmPassword: evt.target.value})
+  // }
 
 
   checkPasswordMatch() {
@@ -20,11 +36,24 @@ export default class Signup extends Component {
         $("#password_message").html("");
   }
 
+  onUserSubmit(event) {
+    event.preventDefault();
+    const userCred = {
+        email: event.target.email.value,
+        password: event.target.new_password.value,
+        first_name: event.target.first_name.value,
+        last_name: event.target.last_name.value,
+    }
+      this.props.registerUser(userCred);
+      // hashHistory.push('/profile')
+
+  }
+
   render() {
     return (
       <div className={styles.container} >
           <div className="row col s12">
-            <form className="col s12">
+            <form className="col s12" >
               <div className="row">
                 <div className="col 12">
                   <Link to="/"><button className="btn-floating btn-large waves-effect waves-light cyan"><i className="material-icons">chevron_left</i></button></Link>
@@ -46,12 +75,13 @@ export default class Signup extends Component {
               </div>
               <div className="row">
                 <div className="input-field col s12">
-                  <input placeholder="Password" id="password" type="password" className="validate" required />
+                  <input placeholder="Password" id="new_password" type="password" className="validate" required />
                 </div>
               </div>
               <div className="row">
                 <div className="input-field col s12">
-                  <input placeholder="Confirm Password" id="password" type="password" className="validate" required />
+                  <h5 id="password_message"></h5>
+                  <input placeholder="Confirm Password" id="confirm_password" type="password" className="validate" onChange={this.checkPasswordMatch} required />
                 </div>
               </div>
               <div className="row">
@@ -63,3 +93,22 @@ export default class Signup extends Component {
     );
   }
 }
+
+/* ---------------- CONTAINER --------------------*/
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+        registerUser: (userCred) => {
+            dispatch(createUser(userCred))
+        }
+        
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(Login);
+
+
