8236ea917ac7ac2b790decc25d87611ce2b22db1
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-11-25 13:44:41 -0500

    changed accordion

--- app/components/Project_List.js
+++ app/components/Project_List.js
@@ -4,22 +4,70 @@ import { connect } from 'react-redux';
 import Collapsible from 'react-collapsible';
 import { Accordion, AccordionItem } from 'react-sanfona';
 
+
+const dummyCommitData = [
+    {
+      committer: "Bonnie Lee",
+      commitMessage: "I updated something",
+      date: "12/10"
+    },
+     {
+      committer: "Adam Orange",
+      commitMessage: "Made a change",
+      date: "12/09"
+    },
+     {
+      committer: "Adam Orange",
+      commitMessage: "Made a change",
+      date: "12/09"
+    },
+     {
+      committer: "Adam Orange",
+      commitMessage: "Made a change",
+      date: "12/09"
+    },
+     {
+      committer: "Adam Orange",
+      commitMessage: "Made a change",
+      date: "12/09"
+    }
+]
+
+
+
 export class Project_List extends Component {
   render() {
     return (
         <div>
           <div className="card-panel orange lighten-2">
-            <h3><i className="material-icons">library_add</i> Project </h3>
+            <h3 className="left-justified-text"><i className="small material-icons">note_add</i> Project </h3>
           </div>
-          <Accordion allowMultiple>
+          <Accordion allowMultiple={false}>
+                {/* LOOP OVER USERS PROJECTS HERE */}
                 {[1, 2, 3, 4, 5].map((item) => {
+
+                  const titleBar = (
+                      <div className="project-title">
+                        <span>Project { item }</span> {/* PULL IN THE PROJECT TITLE HERE */}
+                        <span style={{float: 'right'}} onClick={(evt) => {
+                          alert('this action needs to be changed to re render the collaborators page') // ** ADD ACTION TO RENDER THE "COLLABORATORS" VIEW HERE, AKA REPLACE THE ALERT **
+                          evt.stopPropagation() // **LEAVE THIS HERE!** it makes sure we don't trigger AccordionItemTitle onClick of the icon
+                        }}><i className="small material-icons">supervisor_account</i></span>
+                      </div>
+                    )
+
                     return (
-                        <AccordionItem title={`Project ${ item } Name`} slug={item} key={item} className="card-panel cyan left-justified-text">
-                            <div className="card-panel special-card">
-                                {`Item ${ item } content`}
-                                {item === 3 ? <p><img src="https://cloud.githubusercontent.com/assets/38787/8015584/2883817e-0bda-11e5-9662-b7daf40e8c27.gif" /></p> : null}
-                            </div>
+                        <AccordionItem title={titleBar}  slug={item} key={item} className="card-panel left-justified-text">
+                                {dummyCommitData.map(commitInfo => {
+                                  return (
+                                    <div className="item-commit">
+                                      <div className="commit-message commit-color">{commitInfo.commitMessage}</div>
+                                      <div><span className="commit-message commit-info-font commit-date">{`On ${commitInfo.date}`}</span><span className="commit-info-font">{`by ${commitInfo.committer}`}</span></div>
+                                    </div>
+                                    )
+                                })}
                         </AccordionItem>
+
                     );
                 })}
             </Accordion>
