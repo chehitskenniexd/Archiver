692ff44090ee612479e73e405576c9b17902599d
Kenneth Moy <moykenneth91@gmail.com>
2016-11-25 18:13:41 -0500

    database correctly seeding with  npm run seed

--- db/index.js
+++ db/index.js
@@ -14,48 +14,13 @@ console.log(chalk.yellow(`Hello, Opening database connection to ${url}`));
 // require the db instance
 const db = require('../db/db');
 
-const seeds = require('../db/seeds');
-const blob = seeds.blob,
-      commits = seeds.commit,
-      file = seeds.file,
-      projects = seeds.project,
-      users = seeds.user,
-      user_project = seeds.user_project;
-
 // pull in our models
 require('../db/models');
 
 
-// seed functions
-const seedUser = () => db.Promise.map(users,user => db.model('user').create(user));
-const seedProject = () => db.Promise.map(projects,project => db.model('project').create(project));
-const seedCommit = () => db.Promise.map(commits,commit => db.model('commit').create(commit));
-
-
 // sync the db, creating it if necessary
-db.sync({ force: false })
+db.sync({ force: true })
   .then(ok => console.log(chalk.green(`Synced models to db ${url}`)))
-  /*------------UNCOMMENT TO RESEED DB -----------*/
-  // .then(seedUser)
-  // .then(users => {
-  //   // db.query(alterSequence('users', users.length));
-  //   console.log(`Seeded ${users.length} users OK`)
-  // })
-  // .then(seedProject)
-  // .then(projects => {
-  //     // db.query(alterSequence('projects', projects.length));
-  //     console.log(`Seeded ${projects.length} projects OK`);
-  //   })
-  // .then(user_project)
-  // .then(seedCommit)
-  // .then(commits => {
-  //   // db.query(alterSequence('product', commits.length));
-  //   console.log(`Seeded ${commits.length} commits OK`);
-  // })
-  // .then(blob)
-  // .then(file)
-  // .then(files => console.log(`Seeded ${files.length} files OK`))
-    /*------------END OF UNCOMMENT TO RESEED DB -----------*/
   .catch(fail => {
     console.error(chalk.red('Failed db sync' + fail))
     // Otherwise, do this autocreate nonsense
@@ -64,5 +29,3 @@ db.sync({ force: false })
       require('child_process').exec(`createdb "${name}"`, resolve)
     ).then(() => db.sync({ force: true }))
   });
-
-
--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,21 @@
-let dbRun = require('./index.js');
+let db = require('./db.js');
 const seeds = require('./seeds');
 const blob = seeds.blob,
-      commit = seeds.commit,
-      file = seeds.file,
-      project = seeds.project,
-      user = seeds.user,
-      user_project = seeds.user_project;
+  commits = seeds.commit,
+  file = seeds.file,
+  projects = seeds.project,
+  users = seeds.user,
+  user_project = seeds.user_project;
 
+// seed functions
+const seedUser = () => db.Promise.map(users, user => require('./models/user').create(user));
+// const seedUser = () => db.Promise.map(users, user => db.model('user').create(user));
+const seedProject = () => db.Promise.map(projects, project => require('./models/project').create(project));
+// const seedProject = () => db.Promise.map(projects, project => db.model('project').create(project));
+const seedCommit = () => db.Promise.map(commits, commit => require('./models/commit').create(commit));
+// const seedCommit = () => db.Promise.map(commits, commit => db.model('commit').create(commit));
 
-dbRun.didSync
-  .then(() => dbRun.sync({force: true}))
+db.sync({ force: true })
   .then(seedUser)
   .then(users => {
     // db.query(alterSequence('users', users.length));
@@ -17,9 +23,9 @@ dbRun.didSync
   })
   .then(seedProject)
   .then(projects => {
-      // db.query(alterSequence('projects', projects.length));
-      console.log(`Seeded ${projects.length} projects OK`);
-    })
+    // db.query(alterSequence('projects', projects.length));
+    console.log(`Seeded ${projects.length} projects OK`);
+  })
   .then(user_project)
   .then(seedCommit)
   .then(commits => {
@@ -30,4 +36,3 @@ dbRun.didSync
   .then(file)
   .then(files => console.log(`Seeded ${files.length} files OK`))
   .catch(error => console.error(error))
-  .finally(() => dbRun.close())
