3a56e29fd7c0f8c006c348d9a40086a120cde03c
Kenneth Moy <moykenneth91@gmail.com>
2016-11-27 09:38:46 -0500

    commit to show issues

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -3,12 +3,22 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import styles from './Sidebar.css';
 import Project_List from './Project_List';
-import {fetchUserProjects} from '../reducers/projects_list';
+import { fetchUserProjects } from '../reducers/projects_list';
+import * as Actions from '../../utilities/actions';
 
 export class Sidebar extends Component {
 
- componentDidUpdate(){
-    if (this.props.loginUser.id && !Object.keys(this.props.projects).length){
+  constructor(props) {
+    super(props);
+    this.onClickArchiveUpdate = this.onClickArchiveUpdate.bind(this);
+  }
+
+  onClickArchiveUpdate(event) {
+
+  }
+
+  componentDidUpdate() {
+    if (this.props.loginUser.id && !Object.keys(this.props.projects).length) {
       this.props.onLoadProjects(this.props.loginUser.id);
     }
   }
@@ -16,39 +26,43 @@ export class Sidebar extends Component {
   render() {
     console.log("THIS DOT PROPS", this.props)
     return (
-        <div className={styles.container} >
-            <div className="row">
-              <div className="col s12">
-              <br/>
-              <br/>
-                <Link to="/">
-                  <button className="btn-floating btn-large waves-effect waves-light cyan left">
-                    <i className="material-icons">chevron_left</i>
-                  </button>
-                </Link>
-                <br />
-                <br />
-              </div>
+      <div className={styles.container} >
+        <div className="row">
+          <div className="col s12">
+            <br />
+            <br />
+            <Link to="/">
+              <button className="btn-floating btn-large waves-effect waves-light cyan left">
+                <i className="material-icons">chevron_left</i>
+              </button>
+            </Link>
+            <button className="btn-floating btn-large waves-effect waves-light cyan left"
+              onClick={this.onClickArchiveUpdate}>
+              <i className="material-icons">queue</i>
+            </button>
+            <br />
+            <br />
+          </div>
 
-              <div>
-                <i className="material-icons large">assignment_ind</i>
-                <h6>Settings</h6>
-                <h6>Logout</h6>
-              </div>
+          <div>
+            <i className="material-icons large">assignment_ind</i>
+            <h6>Settings</h6>
+            <h6>Logout</h6>
+          </div>
 
 
-              <div>
-                <Project_List/>
-              </div>
-            </div>
+          <div>
+            <Project_List />
+          </div>
         </div>
+      </div>
     );
   }
 }
 
 
 /* ---------------- CONTAINER --------------------*/
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
     loginUser: state.login,
     projects: state.projects
@@ -57,13 +71,13 @@ function mapStateToProps(state){
 
 function mapDispatchToProps(dispatch) {
   return {
-     onLoadProjects: function (user){
+    onLoadProjects: function (user) {
       dispatch(fetchUserProjects(user));
     }
   }
 }
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Sidebar);
